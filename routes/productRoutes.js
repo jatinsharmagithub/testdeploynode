d0dc5a355b35972404dcd03af8458c616f802c18
jatinsharmagithub <jatisharma874@gmail.com>
2024-08-09 18:34:48 +0530

    first commit  depkoy node

---
+++ routes/productRoutes.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const router = express.Router();
+const Product = require('../model/Product');
+
+// Create a product
+router.post('/', async (req, res) => {
+  try {
+    const product = new Product(req.body);
+    await product.save();
+    res.status(201).json(product);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// Get all products
+router.get('/', async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Update a product
+router.put('/:id', async (req, res) => {
+  try {
+    console.log(` prodcut details: ${req.params.id}`);
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(product);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// Delete a product
+router.delete('/:id', async (req, res) => {
+  try {
+    await Product.findByIdAndDelete(req.params.id);
+    res.json({ message: 'Product deleted' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+module.exports = router;
